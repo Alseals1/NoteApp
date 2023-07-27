b2bcea8846ad56154e8d04c868cfbadbf7b68a53
Alandis <1seals.alandis@gmail.com>
2023-07-27 10:50:11 -0500

    Project setup

---
+++ NoteApp/NoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteAppApp.swift
+//  NoteApp
+//
+//  Created by Alandis Seals on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct NoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
